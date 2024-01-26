6ecba640cea6eee3c7a6d7c05c52f6a2a7205c9a
Maximebond007 <Maximeh2003@gmail.com>
2024-01-26 16:55:12 +0100

    dernier transfert de tout les fichiers
    1er commit pour pull le site en ligne

---
+++ HTML/myWeb.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>MyWebExperience</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="../CSS/styleWeb.css">
+    </head>
+    <body>
+        <header>
+            <nav>
+                <ul>
+                    <li><a href="https://www.fiverr.com/maxclayton2k23/currently-working-on-a-roleplaying-website">Fiverr</a></li>
+                    <li><a href="https://maximeh2003.github.io/PortfolioMH/">Portfolio</a></li>
+                    <li><a href="https://maximeh2003.github.io/jeu-js/">Jeu JS</a></li>
+                    <li><a href="https://maximeh2003.github.io/Action-Calendar/HTML/index.html">Projet Calendrier Infirmerie</a></li>
+                </ul>
+                <li><a href="">Projet Calendrier Infirmerie</a></li>
+            </nav>
+        </header>       
+        <div id="container">
+            <div>
+                
+            </div>
+
+        </div>
+                    
+    
+        <script src="" async defer></script>
+    </body>
+</html>
+
